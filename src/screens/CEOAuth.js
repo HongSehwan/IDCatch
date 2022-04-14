0463e0cc0c0ef651d70de94bfc4cfb0159762940
Hong <gg9297@gmail.com>
2022-04-14 23:58:51 +0900

    Fixed SimplePassword data

--- src/screens/CEOAuth.js
+++ src/screens/CEOAuth.js
@@ -1,36 +1,33 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components/native'
-import HomeBtn from '../components/Idcard/Camera/HomeBtn'
+import React, { useEffect } from "react";
+import styled from "styled-components/native";
+import HomeBtn from "../components/Idcard/Camera/HomeBtn";
 
 const Container = styled.View`
-  flex: 1;
-  background-color: white;
-`
+    flex: 1;
+    background-color: white;
+`;
 
 const LogoImg = styled.Image`
-  height: 65%;
-  width: 100%;
-`
+    height: 65%;
+    width: 100%;
+`;
 
 const CameraBtn = styled.View`
-  align-items: center;
-  justify-content: space-between;
-  margin: 0;
-  width: 100%;
-`
+    align-items: center;
+    justify-content: space-between;
+    margin: 0;
+    width: 100%;
+`;
 
 const CEOAuth = () => {
-  return (
-    <Container>
-      <CameraBtn>
-        <LogoImg
-          resizeMode="stretch"
-          source={require('../assets/img/IDCatch_logo.png')}
-        />
-        <HomeBtn img="ceocamera" text="사업자등록증 촬영하기" />
-      </CameraBtn>
-    </Container>
-  )
-}
+    return (
+        <Container>
+            <CameraBtn>
+                <LogoImg resizeMode="stretch" source={require("../assets/img/IDCatch_logo.png")} />
+                <HomeBtn img="ceocamera" text="사업자등록증 촬영하기" />
+            </CameraBtn>
+        </Container>
+    );
+};
 
-export default CEOAuth
+export default CEOAuth;
